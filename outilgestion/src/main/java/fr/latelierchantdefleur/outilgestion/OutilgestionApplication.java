367aa989569a904317d450f4e5477f0415a7726a
CAYON GLAYERE Nicolas <nicolas.cayonglayere@berger-levrault.com>
2019-05-16 15:38:20 +0200

    structuration du projet springboot

---
+++ outilgestion/src/main/java/fr/latelierchantdefleur/outilgestion/OutilgestionApplication.java
@@ -0,0 +1,13 @@
+package fr.latelierchantdefleur.outilgestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutilgestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutilgestionApplication.class, args);
+	}
+
+}
